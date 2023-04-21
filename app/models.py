c429024bca68168448216df85727fb09c6048641
HenrySpock <motiviquestudio@gmail.com>
2023-04-21 06:29:39 -0500

    touchup

--- app/models.py
+++ app/models.py
@@ -6,14 +6,7 @@ db = SQLAlchemy()
 
 def connect_db(app):
     db.app = app
-    db.init_app(app)
-
-# class User(db.Model):
-#     __tablename__ = 'users'
-#     id = db.Column(db.Integer, primary_key=True)
-#     first_name = db.Column(db.String, nullable=False)
-#     last_name = db.Column(db.String, nullable=False)
-#     image_url = db.Column(db.String, default='default_profile.png')
+    db.init_app(app) 
 
 class User(db.Model):
     """User."""
